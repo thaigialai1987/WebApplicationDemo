9f65054ea83635d81c411cfc8745e9370d956654
thaigialai1987 <thaigialai1987@gmail.com>
2015-07-22 00:09:22 +0700

    first

---
+++ WebApplicationDemo/Models/MeViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationDemo.Models
+{
+    // Models returned by MeController actions.
+    public class GetViewModel
+    {
+        public string Hometown { get; set; }
+    }
+}
